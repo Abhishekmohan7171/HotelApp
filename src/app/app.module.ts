d32ffb4eeae9dc47eb76b686ba6758648c40d893
@abhishek.mohan <abhishek.mohan@surboard.se>
2024-01-18 15:25:57 +0530

    added structure and set the routing for landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './landing-page/navbar/navbar.component';
+import { WorksComponent } from './landing-page/works/works.component';
+import { DiningComponent } from './landing-page/dining/dining.component';
+import { AboutUsComponent } from './landing-page/about-us/about-us.component';
+import { ContactComponent } from './landing-page/contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './landing-page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    WorksComponent,
+    DiningComponent,
+    AboutUsComponent,
+    ContactComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
